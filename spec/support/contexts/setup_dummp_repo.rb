13c51134bcc773edb2ada795af0c18155838b4f1
sue445 <sue445@sue445.net>
2019-05-16 09:00:59 +0900

    Disable Style/FrozenStringLiteralComment in dummy app
    Set default EnforcedStyle to always option for Style/FrozenStringLiteralComment since rubocop 0.69.0
    https://github.com/rubocop-hq/rubocop/blob/master/CHANGELOG.md#0690-2019-05-13

--- spec/support/contexts/setup_dummp_repo.rb
+++ spec/support/contexts/setup_dummp_repo.rb
@@ -5,6 +5,7 @@ RSpec.shared_context 'setup dummy repo' do
     Dir.glob("#{spec_dir}/dummy/*.rb").each do |src|
       FileUtils.cp(src, '.')
     end
+    FileUtils.cp("#{spec_dir}/dummy/.rubocop.yml", '.')
 
     system! 'git init'
     system! 'git add .'
