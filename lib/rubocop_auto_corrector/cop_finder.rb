c36e4f2faf6afe50ab63fb04de6a8bef692003c1
sue445 <sue445@sue445.net>
2024-06-14 23:29:23 +0900

    Support multiple candidacies

--- lib/rubocop_auto_corrector/cop_finder.rb
+++ lib/rubocop_auto_corrector/cop_finder.rb
@@ -12,91 +12,109 @@ module RubocopAutoCorrector
     # Whether this cop is auto correctable
     # @return [Boolean]
     def auto_correctable?
-      Object.new.instance_eval <<-RUBY, __FILE__, __LINE__ + 1
-        # begin
-        #   require 'rubocop-rspec'
-        # rescue LoadError
-        # end
-        #
-        # return ::RuboCop::Cop::RSpec::AlignLeftLetBrace.support_autocorrect? if ::RuboCop::Cop::RSpec::AlignLeftLetBrace.respond_to?(:support_autocorrect?)
-        # ::RuboCop::Cop::RSpec::AlignLeftLetBrace.new.respond_to?(:autocorrect)
+      cop_candidacies.any? do |cop_candidacy|
+        gem_name = cop_candidacy[:gem_name]
+        cop_class_name = cop_candidacy[:cop_class_name]
 
-        begin
-          require '#{gem_name}'
-        rescue LoadError
-        end
-
-        return #{cop_class_name}.support_autocorrect? if #{cop_class_name}.respond_to?(:support_autocorrect?)
-        #{cop_class_name}.new.respond_to?(:autocorrect)
-      RUBY
-    rescue NameError
-      false
+        Object.new.instance_eval <<-RUBY, __FILE__, __LINE__ + 1
+          # begin
+          #   require 'rubocop-rspec'
+          # rescue LoadError
+          # end
+          #
+          # return ::RuboCop::Cop::RSpec::AlignLeftLetBrace.support_autocorrect? if ::RuboCop::Cop::RSpec::AlignLeftLetBrace.respond_to?(:support_autocorrect?)
+          # ::RuboCop::Cop::RSpec::AlignLeftLetBrace.new.respond_to?(:autocorrect)
+  
+          begin
+            require '#{gem_name}'
+          rescue LoadError
+          end
+  
+          return #{cop_class_name}.support_autocorrect? if #{cop_class_name}.respond_to?(:support_autocorrect?)
+          #{cop_class_name}.new.respond_to?(:autocorrect)
+        RUBY
+      rescue NameError
+        false
+      end
     end
 
     # @return [String]
     def gem_name
-      rubocop_cop_info[0][:gem_name]
+      cop_candidacies[0][:gem_name]
     end
 
     # @return [String]
     def cop_class_name
-      rubocop_cop_info[0][:cop_class_name]
+      cop_candidacies[0][:cop_class_name]
     end
-
-    private
 
     # rubocop:disable Metrics/MethodLength
     # @return [Array<Hash<Symbol, String>>]
-    def rubocop_cop_info
-      return @rubocop_cop_info if @rubocop_cop_info
-
+    def cop_candidacies
       cop_class_suffix = cop_name.gsub('/', '::')
 
-      @rubocop_cop_info =
-        case cop_name
-        when %r{^RSpec/}
-          [
-            {
-              gem_name: 'rubocop-rspec',
-              cop_class_name: "::RuboCop::Cop::#{cop_class_suffix}",
-            },
-          ]
-        when 'Rails/HttpStatus'
-          [
-            # for rubocop-rspec < 2.28.0
-            {
-              gem_name: 'rubocop-rspec',
-              cop_class_name: "::RuboCop::Cop::RSpec::#{cop_class_suffix}",
-            },
-          ]
-        when %r{^(FactoryBot|Capybara)/}
-          [
-            # for rubocop-rspec < 2.0.0
-            {
-              gem_name: 'rubocop-rspec',
-              cop_class_name: "::RuboCop::Cop::RSpec::#{cop_class_suffix}",
-            },
-          ]
-        when %r{^(Layout|Lint|Metrics|Naming|Security|Style|Bundler|Gemspec)/}
-          # Official cops
-          [
-            {
-              gem_name: 'rubocop',
-              cop_class_name: "::RuboCop::Cop::#{cop_class_suffix}",
-            }
-          ]
-        else
-          # Unknown cops
-          department_camel = cop_name.split('/').first
-          department_snake = department_camel.gsub(/(?<=.)([A-Z])/) { |s| "_#{s}" }.downcase
+      case cop_name
+      when %r{^RSpec/}
+        [
+          {
+            gem_name: 'rubocop-rspec',
+            cop_class_name: "::RuboCop::Cop::#{cop_class_suffix}",
+          },
+        ]
+      when 'Rails/HttpStatus'
+        [
+          # for rubocop-rspec < 2.28.0
+          {
+            gem_name: 'rubocop-rspec',
+            cop_class_name: "::RuboCop::Cop::RSpec::#{cop_class_suffix}",
+          },
+        ]
+      when %r{^FactoryBot/}
+        [
+          # for rubocop-rspec < 2.0.0
+          {
+            gem_name: 'rubocop-rspec',
+            cop_class_name: "::RuboCop::Cop::RSpec::#{cop_class_suffix}",
+          },
+          # for rubocop-rspec v3+
+          {
+            gem_name: 'rubocop-factory_bot',
+            cop_class_name: "::RuboCop::Cop::#{cop_class_suffix}",
+          },
+        ]
+      when %r{^Capybara/}
+        [
+          # for rubocop-rspec < 2.0.0
+          {
+            gem_name: 'rubocop-rspec',
+            cop_class_name: "::RuboCop::Cop::RSpec::#{cop_class_suffix}",
+          },
+          # for rubocop-rspec v3+
+          {
+            gem_name: 'rubocop-capybara',
+            cop_class_name: "::RuboCop::Cop::#{cop_class_suffix}",
+          },
+        ]
+      when %r{^(Layout|Lint|Metrics|Naming|Security|Style|Bundler|Gemspec)/}
+        # Official cops
+        [
+          {
+            gem_name: 'rubocop',
+            cop_class_name: "::RuboCop::Cop::#{cop_class_suffix}",
+          }
+        ]
+      else
+        # Unknown cops
+        department_camel = cop_name.split('/').first
+        department_snake = department_camel.gsub(/(?<=.)([A-Z])/) { |s| "_#{s}" }.downcase
 
-          [
-            {
-              gem_name: "rubocop-#{department_snake}",
-              cop_class_name: "::RuboCop::Cop::#{cop_class_suffix}",
-            }
-          ]
-        end
+        [
+          {
+            gem_name: "rubocop-#{department_snake}",
+            cop_class_name: "::RuboCop::Cop::#{cop_class_suffix}",
+          }
+        ]
+      end
     end
     # rubocop:enable Metrics/MethodLength
   end
